65a7de9bc833541132744de5fe7d5b27c963234d
rohitpratapitm <rohitpratapitm@gmail.com>
2020-02-24 15:31:35 -0500

    Converted to String

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import {Set} from 'immutable';
+import { Component } from "@angular/core";
+import { Set } from "immutable";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
   // passing NUMBERS in a set of STRING
-  wrongSet: Set<string> = Set<string>([1,2,3]);
-  correctSet: Set<string> = Set<string>(['1','2','3']);
+  wrongSet: Set<string> = Set<string>([1, 2, 3]);
+  correctSet: Set<string> = Set<string>(["1", "2", "3"]);
+
+  constructor() {
+   AppComponent.toStringSet(this.wrongSet);
+   AppComponent.toStringSet(this.correctSet);
+  }
+
+  static toStringSet(set: Set<string>): Set<string> {
+
+    if (typeof set.first() === "number") {
+      console.log("Need Conversion");
+      const mutableArray: string[] = set.toArray();
+      mutableArray.forEach((value, index, mutableArray) => {
+        mutableArray[index] = value.toString();
+      });
+      set = Set<string>(mutableArray);
+      console.log('Successfully converted to ' + typeof set.first());
+    } else {
+      console.log('No need of conversion.');
+    }
+    return set;
+  }
 }
